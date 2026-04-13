a6b72683453f3de94b7da76fcd3d6ef092ca97f5
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-04-13 04:01:17 +0200

    Compatibility with earlier mc versions

--- src/main/java/com/pekar/callofcompanions/items/ItemRegistry.java
+++ src/main/java/com/pekar/callofcompanions/items/ItemRegistry.java
@@ -1,7 +1,6 @@
 package com.pekar.callofcompanions.items;
 
 import com.pekar.callofcompanions.Main;
-import com.pekar.callofcompanions.utils.Resources;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -11,12 +10,13 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import static com.pekar.callofcompanions.Main.MODID;
 import static com.pekar.callofcompanions.utils.Resources.createResourceLocation;
 
+@SuppressWarnings({"removal"})
 public class ItemRegistry
 {
     public static final TagKey<Item> CALL_CRYSTALS_TAG = TagKey.create(Registries.ITEM, createResourceLocation(MODID, "call_crystals"));
 
-    public static final DeferredItem<Item> CALL_CRYSTAL = Main.ITEMS.registerItem("call_crystal", CallCrystal::new, p -> p.stacksTo(1).rarity(Rarity.UNCOMMON));
-    public static final DeferredItem<Item> DEEP_CALL_CRYSTAL = Main.ITEMS.registerItem("deep_call_crystal", DeepCallCrystal::new, p -> p.stacksTo(1).rarity(Rarity.RARE));
+    public static final DeferredItem<Item> CALL_CRYSTAL = Main.ITEMS.registerItem("call_crystal", CallCrystal::new, new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON));
+    public static final DeferredItem<Item> DEEP_CALL_CRYSTAL = Main.ITEMS.registerItem("deep_call_crystal", DeepCallCrystal::new, new Item.Properties().stacksTo(1).rarity(Rarity.RARE));
 
     public static void initStatic()
     {
