6a807c3347b005d587c666bc5f99ee3317353b60
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-04-24 17:28:52 +0200

    **Axolotls** can now only teleport into water.
    (cherry picked from commit 37cf763ced039eb161b75427e70b0e2ac2e94f92)

--- src/main/java/com/pekar/callofcompanions/controllers/animal/TeleportSafetyCheckerResolver.java
+++ src/main/java/com/pekar/callofcompanions/controllers/animal/TeleportSafetyCheckerResolver.java
@@ -1,13 +1,15 @@
 package com.pekar.callofcompanions.controllers.animal;
 
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.axolotl.Axolotl;
 import net.minecraft.world.entity.monster.Strider;
 
 public class TeleportSafetyCheckerResolver
 {
     public static TeleportSafetyChecker getChecker(Animal animal)
     {
-        if (animal instanceof Strider) return new StriderTeleportSafetyChecker();
+        if (animal instanceof Axolotl) return new WaterAnimalTeleportSafetyChecker();
+        else if (animal instanceof Strider) return new StriderTeleportSafetyChecker();
         else return new GroundAnimalTeleportSafetyChecker();
     }
 }
