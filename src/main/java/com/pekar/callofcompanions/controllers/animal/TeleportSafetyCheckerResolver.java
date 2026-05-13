ffc383d9b6d3ddf2b4df4891587e0b4bb9e47ad3
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-05-13 21:29:48 +0200

    Improve teleport safety checker resolution for aquatic and lava-capable animals

--- src/main/java/com/pekar/callofcompanions/controllers/animal/TeleportSafetyCheckerResolver.java
+++ src/main/java/com/pekar/callofcompanions/controllers/animal/TeleportSafetyCheckerResolver.java
@@ -1,18 +1,31 @@
 package com.pekar.callofcompanions.controllers.animal;
 
+import net.minecraft.world.entity.ai.navigation.WaterBoundPathNavigation;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.axolotl.Axolotl;
 import net.minecraft.world.entity.animal.happyghast.HappyGhast;
 import net.minecraft.world.entity.animal.nautilus.AbstractNautilus;
 import net.minecraft.world.entity.monster.Strider;
+import net.minecraft.world.level.pathfinder.PathType;
 
 public class TeleportSafetyCheckerResolver
 {
     public static TeleportSafetyChecker getChecker(Animal animal)
     {
-        if (animal instanceof AbstractNautilus || animal instanceof Axolotl) return new WaterAnimalTeleportSafetyChecker();
-        else if (animal instanceof HappyGhast) return new GhastTeleportSafetyChecker();
-        else if (animal instanceof Strider) return new StriderTeleportSafetyChecker();
-        else return new GroundAnimalTeleportSafetyChecker();
+        if (animal instanceof AbstractNautilus
+                || animal instanceof Axolotl
+                || animal.getNavigation() instanceof WaterBoundPathNavigation
+                || (animal.getPathfindingMalus(PathType.WATER) == 0f && animal.getPathfindingMalus(PathType.WALKABLE) != 0f)
+        )
+            return new WaterAnimalTeleportSafetyChecker();
+
+        else if (animal instanceof HappyGhast)
+            return new GhastTeleportSafetyChecker();
+        else if (animal instanceof Strider
+                || (animal.getPathfindingMalus(PathType.LAVA) == 0f && animal.getPathfindingMalus(PathType.WALKABLE) != 0f)
+        )
+            return new StriderTeleportSafetyChecker();
+        else
+            return new GroundAnimalTeleportSafetyChecker();
     }
 }
