e9816ffd4326f2d94148e4abe55fdc9a9f2f8341
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-05-13 23:17:32 +0200

    Improve teleport safety checker resolution for aquatic and lava-capable animals. Striders now always teleport into Lava if possible.

--- src/main/java/com/pekar/callofcompanions/controllers/animal/TeleportSafetyCheckerResolver.java
+++ src/main/java/com/pekar/callofcompanions/controllers/animal/TeleportSafetyCheckerResolver.java
@@ -1,17 +1,46 @@
 package com.pekar.callofcompanions.controllers.animal;
 
+import net.minecraft.world.entity.ai.navigation.WaterBoundPathNavigation;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.HappyGhast;
 import net.minecraft.world.entity.animal.axolotl.Axolotl;
 import net.minecraft.world.entity.monster.Strider;
+import net.minecraft.world.level.pathfinder.PathType;
 
 public class TeleportSafetyCheckerResolver
 {
     public static TeleportSafetyChecker getChecker(Animal animal)
     {
-        if (animal instanceof Axolotl) return new WaterAnimalTeleportSafetyChecker();
-        else if (animal instanceof HappyGhast) return new GhastTeleportSafetyChecker();
-        else if (animal instanceof Strider) return new StriderTeleportSafetyChecker();
-        else return new GroundAnimalTeleportSafetyChecker();
+        if (animal instanceof Axolotl
+                || animal.getNavigation() instanceof WaterBoundPathNavigation
+                || (animal.getPathfindingMalus(PathType.WATER) == 0f && animal.getPathfindingMalus(PathType.WALKABLE) != 0f)
+        )
+            return new WaterAnimalTeleportSafetyChecker();
+
+        else if (animal instanceof HappyGhast)
+            return new GhastTeleportSafetyChecker();
+
+        else if (animal instanceof Strider
+                || (animal.getPathfindingMalus(PathType.LAVA) == 0f && animal.getPathfindingMalus(PathType.WALKABLE) != 0f)
+        )
+            return new LavaAnimalTeleportSafetyChecker();
+
+        else
+            return new GroundAnimalTeleportSafetyChecker();
+    }
+
+    public static TeleportSafetyChecker getAlternativeChecker(Animal animal)
+    {
+        var primaryChecker = getChecker(animal);
+        if (primaryChecker instanceof WaterAnimalTeleportSafetyChecker) return null;
+
+        if (!(primaryChecker instanceof GroundAnimalTeleportSafetyChecker) && animal.getPathfindingMalus(PathType.WALKABLE) == 0f)
+            return new GroundAnimalTeleportSafetyChecker();
+        if (!(primaryChecker instanceof WaterAnimalTeleportSafetyChecker) && animal.getPathfindingMalus(PathType.WATER) == 0f)
+            return new WaterAnimalTeleportSafetyChecker();
+        if (!(primaryChecker instanceof LavaAnimalTeleportSafetyChecker) && animal.getPathfindingMalus(PathType.LAVA) == 0f)
+            return new LavaAnimalTeleportSafetyChecker();
+
+        return null;
     }
 }
