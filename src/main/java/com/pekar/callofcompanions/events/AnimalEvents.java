8d6b655b34f9c936b607fbec18b53f94ceb0811b
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-05-19 20:50:08 +0200

    corrections

--- src/main/java/com/pekar/callofcompanions/events/AnimalEvents.java
+++ src/main/java/com/pekar/callofcompanions/events/AnimalEvents.java
@@ -13,7 +13,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.animal.equine.AbstractHorse;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
