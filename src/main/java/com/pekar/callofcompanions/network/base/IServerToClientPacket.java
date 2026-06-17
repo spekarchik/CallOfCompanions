ab700a9a9ef77c4b6e1382519a616fc9e0e157cf
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-06-18 01:44:20 +0200

    Improved dedicated server compatibility.
    (cherry picked from commit 484ed7f694b3daebe2714669308cd1bed51bd590)
    (cherry picked from commit 47f7556ab729361129a5160c768def11affbe2b1)

--- src/main/java/com/pekar/callofcompanions/network/base/IServerToClientPacket.java
+++ src/main/java/com/pekar/callofcompanions/network/base/IServerToClientPacket.java
@@ -1,8 +1,8 @@
 package com.pekar.callofcompanions.network.base;
 
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
@@ -11,5 +11,5 @@ public interface IServerToClientPacket extends IPacket
     void sendToPlayer(ServerPlayer player);
     void sendToEntity(Entity entity);
     void sendToChunk(LevelChunk chunk);
-    void onReceive(LocalPlayer player);
+    void onReceive(Player player);
 }
