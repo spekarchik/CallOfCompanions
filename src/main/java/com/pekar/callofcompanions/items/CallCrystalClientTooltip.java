19b0137b8b8a9082e1c724457687354229f1cd7f
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-06-17 16:27:38 +0200

    Fixed a dedicated server startup crash on Fabric.
    (cherry picked from commit cc546c13826bc55ecda32009127ee5929bc857a4)

---
+++ src/main/java/com/pekar/callofcompanions/items/CallCrystalClientTooltip.java
@@ -0,0 +1,34 @@
+package com.pekar.callofcompanions.items;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.Level;
+
+@Environment(EnvType.CLIENT)
+final class CallCrystalClientTooltip
+{
+    private CallCrystalClientTooltip()
+    {
+    }
+
+    static Level getLevel()
+    {
+        return Minecraft.getInstance().level;
+    }
+
+    static boolean hasShiftDown()
+    {
+        var window = Minecraft.getInstance().getWindow();
+        return InputConstants.isKeyDown(window, InputConstants.KEY_LSHIFT)
+                || InputConstants.isKeyDown(window, InputConstants.KEY_RSHIFT);
+    }
+
+    static boolean hasAltDown()
+    {
+        var window = Minecraft.getInstance().getWindow();
+        return InputConstants.isKeyDown(window, InputConstants.KEY_LALT)
+                || InputConstants.isKeyDown(window, InputConstants.KEY_RALT);
+    }
+}
